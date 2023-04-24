26c51a80184e0520a87cbfad3bf492f235ebf8aa
tbgvk <tbgvkekana@gmail.com>
2023-04-24 04:28:45 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h> 
 /**
- * main - A program that print a line using the printf function
- * Return 0
- */
+*  * main - Prints out a string to stdout
+*  * Return 0 (If Success)
+*/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
