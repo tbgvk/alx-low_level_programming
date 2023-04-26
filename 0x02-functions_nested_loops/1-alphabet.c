48072372480e8658f057cca1cf12ede77f00be8e
tbgvk <tbgvkekana@gmail.com>
2023-04-26 14:10:16 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,14 @@
-/**
- * print_alphabet - print all alphabet in lowercase
- */
+#include "main.h"
 
+/**
+ * print_alphabet - print all alphabet in lower cases
+ */
 
 void print_alphabet(void)
 {
 	char letter;
 
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-
-
-	_putchar('\n');
+	_putchar("\n");
 }
